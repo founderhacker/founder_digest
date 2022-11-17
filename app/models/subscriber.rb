648cab36ba3887aa5ab4a130a9d50b68c274812a
Ryan Kulp <ryanckulp@gmail.com>
2022-11-17 09:21:40 -0500

    renaming Subscriptions to Subscribers to prevent billing interference

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,4 @@
-class Subscription < ApplicationRecord
+class Subscriber < ApplicationRecord
   belongs_to :project # mom
   belongs_to :user # dad (2 parent objects OK)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   scope :subscribed, -> { where(paying_customer: true) }
 
   has_many :projects
-  has_many :subscriptions
+  has_many :subscribers
 
   has_many :stakeholder_updates, through: :projects
 end
