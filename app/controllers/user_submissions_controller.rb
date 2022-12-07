7b1e81fa51d8c172dfcae4460ebcfc3163442fbe
Ryan Kulp <ryanckulp@gmail.com>
2022-12-07 13:04:20 -0500

    backend - user submission Rails form and endpoint, controller

---
+++ app/controllers/user_submissions_controller.rb
@@ -0,0 +1,15 @@
+class UserSubmissionsController < ApplicationController
+  def create
+    puts "WHITELISTED PARAMS: #{user_submission_params}"
+    # UserSubmission.create!(user_submission_params)
+    # redirect_to root_path
+  end
+
+  # TODO: whitelist the "role" radio button
+  def user_submission_params
+    params.require(:user_submission).permit(:first_name, :last_name, :email, :website, :text)
+  end
+end
+
+# what params will look like!
+# params = {authenticity_token: 'asdfasdf', user_submission: {first_name: 'asdf'}}
